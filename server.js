b5b2f509fd747391da66205051ba73f54355f12f
zilurrane <zilurrane@gmail.com>
2021-09-26 15:10:48 +0530

    inMemory OAuth Model

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 5000
+var bodyParser = require('body-parser');
+var express = require('express');
+var OAuthServer = require('express-oauth-server');
+var memorystore = require('./model.js');
 
-app.get('/', (_req, res) => {
-    res.send('Default route...');
+var app = express();
+
+app.oauth = new OAuthServer({
+  model: new memorystore()
 });
 
-app.get('/protected', (_req, res) => {
-    res.send('Protected route...');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(app.oauth.authorize());
+
+app.use(function(req, res) {
+  res.send('Secret area');
 });
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
+app.listen(5000);
